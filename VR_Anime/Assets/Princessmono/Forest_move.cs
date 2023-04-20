488606103ab78a23ba8b66b68b51bebad9e252c7
Orgzales <77345323+Orgzales@users.noreply.github.com>
2023-04-19 20:42:59 -0400

    Mono_path Made

---
+++ VR_Anime/Assets/Princessmono/Forest_move.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Forest_move : MonoBehaviour
+{
+public List<GameObject> waypoints;
+    public float speed = 2; 
+    // float original_speed = 2;
+    int index = 0;
+    public bool isLoop = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+        Vector3 destination = waypoints[index].transform.position;
+        Vector3 newPos = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
+        transform.position = newPos;
+
+        float distance = Vector3.Distance(transform.position, destination);
+        if(distance <= 0.05)
+        {
+            // speed = original_speed;
+            if(index < waypoints.Count-1)
+            {
+                index++;
+            }
+            // if(index == 1){
+            //     speed = 15;
+            // }
+        }
+        else
+        {
+            if(isLoop & index == waypoints.Count-1)
+            {
+                index= 0;
+            }
+        }
+
+    }   
+}
